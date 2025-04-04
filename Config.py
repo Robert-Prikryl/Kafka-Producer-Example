7a59d2f9fcd4e2402602d3eca655ae26bc8bc89d
RobertPrikryl <prikryl.robo@gmail.com>
2025-04-04 11:48:16 +0200

    chore: updated config with sasl auth

--- Config.py
+++ Config.py
@@ -1,21 +1,23 @@
 import socket
 
 conf = {
-    # Kafka broker(s) address
-    'bootstrap.servers': 'localhost:9092',
+    # Kafka broker bootstrap servers (host:port)
+    'bootstrap.servers': '',
+    # Security protocol to use (PLAINTEXT, SSL, SASL_PLAINTEXT, SASL_SSL)
+    'security.protocol': '',
+    # SASL mechanism to use for authentication (PLAIN, GSSAPI, SCRAM-SHA-256, etc)
+    'sasl.mechanism': '',
+    # Username for SASL/PLAIN or SASL/SCRAM authentication
+    'sasl.username': '',
+    # Password for SASL/PLAIN or SASL/SCRAM authentication
+    'sasl.password': '',
 
-    # Client identifier CAN BE REPLACED WITH A UNIQUE IDENTIFIER FOR THE PRODUCER
+    # (Optional) Client identifier CAN BE REPLACED WITH A UNIQUE IDENTIFIER FOR THE PRODUCER
     'client.id': socket.gethostname(),
-
-    # Acknowledgment settings (1: leader acknowledgment)
+    # (Optional) Acknowledgment settings (1: leader acknowledgment) 
     'acks': '1',
-    
-    # Number of retries if the request fails
+    # (Optional) Number of retries if the request fails
     'retries': 3,
-    
-    # Retry backoff time in milliseconds
+    # (Optional) Retry backoff time in milliseconds
     'retry.backoff.ms': 1000,
-    
-    # Maximum size of a request in bytes
-    'message.max.bytes': 1000000
 }
